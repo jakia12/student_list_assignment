bd1fb377d70c859072dd1a01e60904fe39a0a411
jakia12 <owasi110@gmail.com>
2024-01-17 21:23:34 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Banner from "./components/banner/Banner";
-import Footer from "./components/footer/footer";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import TableList from "./components/tableList/TableList";
 
